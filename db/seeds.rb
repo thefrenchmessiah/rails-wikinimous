ac2b61219fa9f0ad402fa5b86ff9093dcca8d38d
elia benedetti <eliabenedetti04@gmail.com>
2023-11-14 16:20:35 +0100

    kinda pretty no?

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+10.times do
+  Article.create(title: Faker::GreekPhilosophers.name, content: Faker::GreekPhilosophers.quote)
+end
